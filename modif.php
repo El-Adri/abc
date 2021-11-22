a61ada3b6e2bafb0117c57b112d167170f097a50
El-Adri <89391746+El-Adri@users.noreply.github.com>
2021-11-22 16:53:10 +0100

    Add files via upload

---
+++ modif.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+ <head>
+	<title>modification
+	</title>
+	<link rel="stylesheet" type="text/css" href="abcr.css">
+ </head>
+ <body>
+
+   <section id="formu"><h3>MISE A JOUR</h3>
+     <form action="index.php" method="post">
+          <textarea rows="4" id="texta" name="adresse" placeholder="Adresse">	
+          </textarea>
+          <input class="ident" type="text" id="box" name="tel"  placeholder="Numéro de téléphone">
+          <br><input class="ident" type="text" id="box" name="email" placeholder="Email">
+          <br><input class="ident" type="text" id="box" name="compte" placeholder="Numéro de compte">
+          <br>
+          <input type="submit" value="Enregistrer" class="button"> 
+     </form> 
+   </section>
+ </body>
+</html>
